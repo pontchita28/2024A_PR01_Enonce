ff44941fa00e92c9fdaf055ec6e9c6dda52f14bd
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2024-09-22 09:35:53 -0400

    Complétion des fonctions create_coins(board) et de create_special_coins(board), avec les pièces qui apparaissent à la bonne position!!!

--- helper.py
+++ helper.py
@@ -27,26 +27,36 @@ def create_board():
 
     return maze
 
-def create_coins(board):
-    coins = []
 
-    for i in range(0, len(board)):
-        for j in range(0, len(board[i])):
-            if board[i][j] == 0:
-                coins.append((i, j))
-
-    
-    # TODO: Ajouter la position de toutes les cases '0' à la variable coins. Pour ajouter un élément, vous pouvez utiliser l'expression suivante :
+# TODO: Ajouter la position de toutes les cases '0' à la variable coins. Pour ajouter un élément, vous pouvez utiliser l'expression suivante :
     # coins.append((x, y))
     # en remplacant x et y par la position. Notez que le premier coin est à la position (1, 1)
 
-    # TODO: Retirer les coins de chaque "coin" du carré. Vous devez utiliser la variable 'special_coins_pos' et la fonction 'remove'.
+def create_coins(board):
+    coins = []
+
+    # for the range of all the lists in the board 
+    for i in range(0, len(board)):
+        # for the range of all the positions in the lists + 1
+            # to not exclude the last number 
+        for j in range(0, len(board[i])+1):
+            # append the position of the i and the j as a tuple
+            # TODO: Retirer les coins de chaque "coin" du carré. 
+            # Vous devez utiliser la variable 'special_coins_pos' 
+            # et la fonction 'remove'.
+            if (i,j) not in special_coins_pos:
+                coins.append((i, j))
 
     return coins
 
+
+# TODO: Ajouter des coins aux positions spéciales, en utilisant la variable 'special_coins_pos'.
 def create_special_coins(board):
     special_coins = []
 
-    # TODO: Ajouter des coins aux positions spéciales, en utilisant la variable 'special_coins_pos'.
+    for i in range(0, len(board)):
+        for j in range(0, len(board)+1):
+            if (i,j) in special_coins_pos:
+                special_coins.append((i,j))
     
     return special_coins
